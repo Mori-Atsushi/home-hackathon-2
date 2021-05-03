8f4bec9cb3a753b93de402a5c655d3c049949d31
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-05-03 17:01:46 +0900

    Add divider

--- app-android/app/src/main/kotlin/com/example/home_hackathon2/ui/initial/InitialScreen.kt
+++ app-android/app/src/main/kotlin/com/example/home_hackathon2/ui/initial/InitialScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,6 +12,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.home_hackathon2.ui.res.COLOR_BLACK
+import com.example.home_hackathon2.ui.res.COLOR_GRAY
 
 @Composable
 fun InitialScreen() {
@@ -28,10 +30,15 @@ fun InitialScreen() {
             text = "ようこそ"
         )
         Text(
+            modifier = Modifier.padding(bottom = 80.dp),
             color = COLOR_BLACK,
             fontSize = 14.sp,
             fontWeight = FontWeight.Bold,
             text = "音声入力で高速なコミュニーケーションを"
+        )
+        Divider(
+            modifier = Modifier.padding(bottom = 24.dp),
+            color = COLOR_GRAY
         )
     }
 }
