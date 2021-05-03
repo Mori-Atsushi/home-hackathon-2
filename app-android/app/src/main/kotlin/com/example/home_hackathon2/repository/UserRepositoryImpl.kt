fcf826559ce2ba50490805704fbb06197223e918
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-05-03 21:45:06 +0900

    Add isCreatedUser variable to UserRepository

--- app-android/app/src/main/kotlin/com/example/home_hackathon2/repository/UserRepositoryImpl.kt
+++ app-android/app/src/main/kotlin/com/example/home_hackathon2/repository/UserRepositoryImpl.kt
@@ -4,16 +4,31 @@ import com.example.home_hackathon2.model.UserWithAuth
 import com.example.home_hackathon2.pb.App
 import com.example.home_hackathon2.pb.AppServiceGrpcKt.AppServiceCoroutineStub
 import com.example.home_hackathon2.repository.mapper.toModel
+import com.example.home_hackathon2.source.Preferences
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 class UserRepositoryImpl @Inject constructor(
-    private val service: AppServiceCoroutineStub
+    private val service: AppServiceCoroutineStub,
+    private val preferences: Preferences
 ) : UserRepository {
+    private val isCreatedUserValue
+        get() = preferences.userId != null && preferences.accessToken != null
+    private val _isCreatedUser = MutableStateFlow(isCreatedUserValue)
+    override val isCreatedUser: StateFlow<Boolean>
+        get() = _isCreatedUser
+
     override suspend fun createUser(name: String): UserWithAuth {
         val request = App.CreateUserRequest.newBuilder()
             .setName(name)
             .build()
         val response = service.createUser(request)
-        return response.userWithAuth.toModel()
+        val model = response.userWithAuth.toModel()
+        preferences.userId = model.user.id
+        preferences.userName = model.user.name
+        preferences.accessToken = model.accessToken
+        _isCreatedUser.value = true
+        return model
     }
 }
