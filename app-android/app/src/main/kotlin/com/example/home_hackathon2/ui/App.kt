4d3a7f4c52f0ae9f08a36ffc6ae87c21c63bb21c
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-05-03 22:32:36 +0900

    Implement to inject AppViewModel

--- app-android/app/src/main/kotlin/com/example/home_hackathon2/ui/App.kt
+++ app-android/app/src/main/kotlin/com/example/home_hackathon2/ui/App.kt
@@ -1,10 +1,21 @@
 package com.example.home_hackathon2.ui
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import com.example.home_hackathon2.ui.initial.InitialScreen
+import com.example.home_hackathon2.ui.tools.rememberViewModel
 
 @Composable
 fun App() {
-    // TODO: 初期かどうか判断する
-    InitialScreen()
+    val appViewModel = rememberViewModel {
+        it.getAppViewModel()
+    }
+    val isVisibleInitialScreen = appViewModel.isVisibleInitialScreen.collectAsState()
+    if (isVisibleInitialScreen.value) {
+        InitialScreen()
+    } else {
+        // TODO: change
+        Text("Hello World")
+    }
 }
