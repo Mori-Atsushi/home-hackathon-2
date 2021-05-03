d503650c60f5374739494f2c20c66cbe6acffc37
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-05-03 18:09:26 +0900

    Add dagger hilt dependencies

--- app-android/build.gradle.kts
+++ app-android/build.gradle.kts
@@ -8,6 +8,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.0.0-alpha15")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.32")
         classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.16")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.32-alpha")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle.kts files
