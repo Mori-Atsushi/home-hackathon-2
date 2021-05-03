fba66daa3494aae44d7c4debc5b8c17c06abb849
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-05-03 12:20:54 +0900

    Add compose dependencies

--- app-android/app/build.gradle.kts
+++ app-android/app/build.gradle.kts
@@ -37,6 +37,14 @@ android {
     sourceSets {
         getByName("main").java.srcDir("src/main/kotlin")
     }
+
+    buildFeatures {
+        compose = true
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.0.0-beta05"
+    }
 }
 
 dependencies {
@@ -45,6 +53,13 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.3.0")
     implementation("org.jetbrains.kotlin:kotlin-stdlib:1.5.0")
+
+    implementation("androidx.compose.ui:ui:1.0.0-beta05")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0-beta05")
+    implementation("androidx.compose.foundation:foundation:1.0.0-beta05")
+    implementation("androidx.compose.material:material:1.0.0-beta05")
+    implementation("androidx.activity:activity-compose:1.3.0-alpha07")
+
     testImplementation("junit:junit:4.+")
     androidTestImplementation("androidx.test.ext:junit:1.1.2")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
