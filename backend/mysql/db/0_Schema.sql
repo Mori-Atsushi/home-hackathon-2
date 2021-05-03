98775547399afe7563b8a704cfb77103d7463a95
Kazuki Chigita <kazuki.chigichan@gmail.com>
2021-05-03 16:51:23 +0900

    Fix config sql

--- backend/mysql/db/0_Schema.sql
+++ backend/mysql/db/0_Schema.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS ouchi.user;
 
 CREATE TABLE ouchi.user
 (
-    id          INTEGER             NOT NULL PRIMARY KEY,
-    name        VARCHAR(64)         NOT NULL,
-    access_token VARCHAR(4096)       NOT NULL
+    uuid            CHAR(36)        NOT NULL PRIMARY KEY,
+    name            VARCHAR(64)     NOT NULL,
+    access_token    CHAR(36)        NOT NULL
 );
