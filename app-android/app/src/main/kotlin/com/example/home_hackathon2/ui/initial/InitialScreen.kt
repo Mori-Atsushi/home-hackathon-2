8e37eb5c45af1c5e640f14dc691bb3fe3fa95234
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-05-04 12:43:14 +0900

    Implement to show error

--- app-android/app/src/main/kotlin/com/example/home_hackathon2/ui/initial/InitialScreen.kt
+++ app-android/app/src/main/kotlin/com/example/home_hackathon2/ui/initial/InitialScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -13,37 +14,45 @@ import com.example.home_hackathon2.ui.res.COLOR_BLACK
 import com.example.home_hackathon2.ui.res.COLOR_DARK
 import com.example.home_hackathon2.ui.res.COLOR_LIGHT
 import com.example.home_hackathon2.ui.tools.rememberViewModel
-import com.example.home_hackathon2.ui.widget.BorderTextField
-import com.example.home_hackathon2.ui.widget.TintButton
-import com.example.home_hackathon2.ui.widget.paddingBottomIme
+import com.example.home_hackathon2.ui.widget.*
+import kotlinx.coroutines.flow.receiveAsFlow
 
 @Composable
 fun InitialScreen() {
     val viewModel = rememberViewModel {
         it.getInitialViewModel()
     }
-    Column(
+    Box(
         modifier = Modifier
-            .fillMaxHeight()
-            .padding(horizontal = 30.dp)
+            .fillMaxSize()
             .paddingBottomIme(),
-        verticalArrangement = Arrangement.Center
     ) {
-        Message(
-            modifier = Modifier.padding(bottom = 80.dp),
-        )
-        Divider(
-            modifier = Modifier.padding(bottom = 24.dp),
-            color = COLOR_LIGHT
-        )
-        NameTextField(
-            text = viewModel.name.collectAsState().value,
-            onValueChange = viewModel::handleChangeName,
-            modifier = Modifier.padding(bottom = 16.dp),
-        )
-        TintButton(
-            text = "はじめる",
-            onClick = viewModel::submit
+        Column(
+            modifier = Modifier
+                .fillMaxHeight()
+                .padding(horizontal = 30.dp),
+            verticalArrangement = Arrangement.Center
+        ) {
+            Message(
+                modifier = Modifier.padding(bottom = 80.dp),
+            )
+            Divider(
+                modifier = Modifier.padding(bottom = 24.dp),
+                color = COLOR_LIGHT
+            )
+            NameTextField(
+                text = viewModel.name.collectAsState().value,
+                onValueChange = viewModel::handleChangeName,
+                modifier = Modifier.padding(bottom = 16.dp),
+            )
+            TintButton(
+                text = "はじめる",
+                onClick = viewModel::submit
+            )
+        }
+        ErrorSnackbarHost(
+            modifier = Modifier.align(Alignment.BottomCenter),
+            messages = viewModel.errorMessage.receiveAsFlow()
         )
     }
 }
--- app-android/app/src/main/kotlin/com/example/home_hackathon2/ui/initial/InitialViewModel.kt
+++ app-android/app/src/main/kotlin/com/example/home_hackathon2/ui/initial/InitialViewModel.kt
@@ -2,6 +2,8 @@ package com.example.home_hackathon2.ui.initial
 
 import com.example.home_hackathon2.ui.tools.ViewModel
 import com.example.home_hackathon2.usecase.CreateUserUseCase
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -13,6 +15,9 @@ class InitialViewModel @Inject constructor(
     private val _name = MutableStateFlow("")
     val name: StateFlow<String> = _name
 
+    private val _errorMessage = Channel<String>(Channel.CONFLATED)
+    val errorMessage: ReceiveChannel<String> = _errorMessage
+
     fun handleChangeName(name: String) {
         _name.value = name
     }
@@ -20,7 +25,17 @@ class InitialViewModel @Inject constructor(
     fun submit() {
         viewModelScope.launch {
             val input = CreateUserUseCase.Input(_name.value)
-            createUserUseCase.invoke(input)
+            when (createUserUseCase.invoke(input)) {
+                is CreateUserUseCase.Output.EmptyNameError -> {
+                    _errorMessage.send("名前は空にできません。")
+                }
+                is CreateUserUseCase.Output.OtherError -> {
+                    _errorMessage.send("エラーが発生しました。")
+                }
+                CreateUserUseCase.Output.Success -> {
+                    // no op
+                }
+            }
         }
     }
 }
