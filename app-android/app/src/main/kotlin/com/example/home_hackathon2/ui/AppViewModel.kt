bea59d6ce73002ad91dc6f3aec0e9a334dde7968
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-05-04 13:01:30 +0900

    Implement isVisibleInitialScreen

--- app-android/app/src/main/kotlin/com/example/home_hackathon2/ui/AppViewModel.kt
+++ app-android/app/src/main/kotlin/com/example/home_hackathon2/ui/AppViewModel.kt
@@ -3,7 +3,10 @@ package com.example.home_hackathon2.ui
 import com.example.home_hackathon2.ui.tools.ViewModel
 import com.example.home_hackathon2.usecase.ObserveUserCreatedUseCase
 import com.example.home_hackathon2.usecase.common.invoke
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
 class AppViewModel @Inject constructor(
@@ -11,4 +14,6 @@ class AppViewModel @Inject constructor(
 ) : ViewModel() {
     val isVisibleInitialScreen: StateFlow<Boolean> =
         observeUserCreatedUseCase.invoke(viewModelScope)
+            .map { !it }
+            .stateIn(viewModelScope, SharingStarted.Eagerly, true)
 }
