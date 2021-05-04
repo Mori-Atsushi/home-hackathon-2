6b68f76fbda3122fae4908c71c10e40450b5dfbc
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-05-05 00:10:59 +0900

    Fix ApplicationScope

--- app-android/app/src/main/kotlin/com/example/home_hackathon2/util/ApplicationScopeImpl.kt
+++ app-android/app/src/main/kotlin/com/example/home_hackathon2/util/ApplicationScopeImpl.kt
@@ -1,10 +1,13 @@
 package com.example.home_hackathon2.util
 
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
+import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
-class ApplicationScopeImpl: ApplicationScope {
+class ApplicationScopeImpl @Inject constructor() : ApplicationScope {
+    private val exceptionHandler = CoroutineExceptionHandler { _, _ -> }
     override val coroutineContext: CoroutineContext =
-        SupervisorJob() + Dispatchers.Main.immediate
+        SupervisorJob() + Dispatchers.Main.immediate + exceptionHandler
 }
