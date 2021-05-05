bcfcf997d4110304205fa6a2ab30b3bba33ba37a
Kazuki Chigita <kazuki.chigichan@gmail.com>
2021-05-05 15:01:34 +0900

    Implement permission dialog

--- app-android/app/src/main/kotlin/com/example/home_hackathon2/ui/chat/ChatRoomScreen.kt
+++ app-android/app/src/main/kotlin/com/example/home_hackathon2/ui/chat/ChatRoomScreen.kt
@@ -8,8 +8,6 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalWindowInfo
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
