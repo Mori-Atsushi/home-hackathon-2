9a342f1c935e4fdd7040b7f6fe4b583220315ed7
Mori-Atsushi <at.sushi.tb@gmail.com>
2021-05-05 13:15:23 +0900

    Add myPendingChat to ChatRoom

--- app-android/app/src/main/kotlin/com/example/home_hackathon2/model/ChatRoom.kt
+++ app-android/app/src/main/kotlin/com/example/home_hackathon2/model/ChatRoom.kt
@@ -1,11 +1,27 @@
 package com.example.home_hackathon2.model
 
 data class ChatRoom(
-    val items: List<ChatRoomItem> = emptyList()
+    val items: List<ChatRoomItem> = emptyList(),
+    val myPendingChat: String? = null
 ) {
     fun added(chat: Chat): ChatRoom {
+        val isPendingChat = chat.isMe && myPendingChat != null && myPendingChat == chat.message
         return copy(
-            items = items + ChatRoomItem.Chat(chat)
+            items = items + ChatRoomItem.Chat(chat),
+            myPendingChat = if (isPendingChat) null else myPendingChat
         )
     }
+
+    fun addedPendingChat(message: String?): ChatRoom {
+        return copy(
+            myPendingChat = message
+        )
+    }
+
+    fun isEmpty(): Boolean {
+        return items.isEmpty() && myPendingChat == null
+    }
+
+    val lastIndex: Int
+        get() = items.lastIndex + if (myPendingChat == null) 0 else 1
 }
